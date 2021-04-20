4f0bbb76c088ad939223ee5ffec2af6a7753ce82
Wei Yang <limweiyang@firstcom.com.sg>
2021-04-20 21:30:28 +0800

    Added get all records function & included api route

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\API;
 
 class ApiController extends Controller
 {
-    //
+    public function getData(){
+        return API::all();
+    }
 }
